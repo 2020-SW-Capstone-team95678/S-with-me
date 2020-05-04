f9d37cd3390df9ad06bd29fe4839ba253fc1baa0
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-04 14:58:40 +0900

    add enzyme library and example test file

--- s-with-me-front/src/setupTests.js
+++ s-with-me-front/src/setupTests.js
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16.3';
+
+configure({ adapter: new Adapter() });
+
+// 경고 메시지를 오류로 인식
+afterEach(() => {
+  console.error.mockClear();
+});
+beforeEach(() => {
+  jest.spyOn(console, 'error').mockImplementation(e => {
+    throw new Error(e);
+  });
+});
