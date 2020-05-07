6a570a8dd488806fa3f6a8f89cb42c70459489bc
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-07 16:07:07 +0900

    edit story

--- s-with-me-front/src/stories/BookOverviewStory.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import BookOverview from '../components/student/libarary/BookOverview';
-
-storiesOf('Library/BookOverview', module).addWithJSX('overview default', () => <BookOverview />);
