aef7c09175d224baf6998d8b1809b1b7a3628c87
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-09 19:10:49 +0900

    optional problem checkbox state 변경되도록

--- s-with-me-front/src/components/student/problem/AnswerInput.jsx
+++ s-with-me-front/src/components/student/problem/AnswerInput.jsx
@@ -1,38 +1,57 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+
 import Input from '../../../common-ui/Input';
 import CheckBox from '../../../common-ui/CheckBox';
+import VerticalList from '../../../common-ui/VerticalList';
 
 export default class AnswerInput extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       options: [
-        { optionNum: 1, optionContent: '1번 보기' },
-        { optionNum: 2, optionContent: '2번 보기' },
-        { optionNum: 3, optionContent: '3번 보기' },
-        { optionNum: 4, optionContent: '4번 보기' },
-        { optionNum: 5, optionContent: '5번 보기' },
+        { optionNum: 1, optionContent: '1번 보기', isChecked: false },
+        { optionNum: 2, optionContent: '2번 보기', isChecked: false },
+        { optionNum: 3, optionContent: '3번 보기', isChecked: false },
+        { optionNum: 4, optionContent: '4번 보기', isChecked: false },
+        { optionNum: 5, optionContent: '5번 보기', isChecked: false },
       ],
+      answer: '',
     };
+    this.setAnswer = this.setAnswer.bind(this);
   }
-  static propTypes = {
-    isOptional: PropTypes.bool,
-  };
 
-  static defaultProps = {
-    isOptional: false,
-  };
-
+  setAnswer(answer) {
+    if (this.props.isOptional) {
+      const { options } = this.state;
+      this.setState({
+        answer: answer,
+        options: options.map(option =>
+          answer === option.optionNum ? { ...option, isChecked: !option.isChecked } : option,
+        ),
+      });
+    } else {
+      this.setState({ answer: answer });
+      console.log(answer);
+    }
+  }
   render() {
-    const { isOptional } = this.props;
-    if (isOptional) {
+    const { options } = this.state;
+    if (this.props.isOptional) {
       return (
-        <div>
-          {this.state.options.map((option, i) => (
-            <CheckBox label={option.optionNum}>{option.optionContent}</CheckBox>
+        <VerticalList spacingBetween={1}>
+          {options.map(option => (
+            <CheckBox
+              name="optionInput"
+              label={option.optionNum}
+              onChange={() => {
+                this.setAnswer(option.optionNum);
+              }}
+              checked={option.isChecked}
+            >
+              {option.optionContent}
+            </CheckBox>
           ))}
-        </div>
+        </VerticalList>
       );
     } else {
       return <Input type="text" name="subjectiveAnswer" label="정답을 입력하세요" />;
