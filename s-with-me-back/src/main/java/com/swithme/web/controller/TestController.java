416281395ac20c951509fe7f81c3fa7366a365b6
sungkyu <kskyu610@gmail.com>
2020-05-14 00:44:35 +0900

    @Controller -> @RestController 오타 수정

--- s-with-me-back/src/main/java/com/swithme/web/controller/TestController.java
+++ s-with-me-back/src/main/java/com/swithme/web/controller/TestController.java
@@ -1,9 +1,9 @@
 package com.swithme.web.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
     @GetMapping("/testPublish")
