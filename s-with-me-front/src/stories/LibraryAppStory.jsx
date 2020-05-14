c4e99e1f436d6fa27b6740bf3f8ca9a87d1cf0b8
정소희 <wjdth@MacBook-Pro.local>
2020-05-14 12:26:25 +0900

    AddBook page 생성

--- s-with-me-front/src/stories/LibraryAppStory.jsx
+++ s-with-me-front/src/stories/LibraryAppStory.jsx
@@ -3,8 +3,10 @@ import { storiesOf } from '@storybook/react';
 import StudentLibraryApp from '../components/student/libarary/LibraryApp';
 import PublisherLibraryApp from '../components/publisher/library/LibraryApp';
 import BookDetail from '../components/student/libarary/BookDetail';
+import AddBook from '../components/publisher/book/AddBook';
 
 storiesOf('Library/LibraryApp', module)
   .addWithJSX('Student', () => <StudentLibraryApp />)
   .addWithJSX('Publiser', () => <PublisherLibraryApp />)
-  .addWithJSX('BookDetail', () => <BookDetail />);
+  .addWithJSX('BookDetail', () => <BookDetail />)
+  .addWithJSX('AddBook',()=> <AddBook/>);
