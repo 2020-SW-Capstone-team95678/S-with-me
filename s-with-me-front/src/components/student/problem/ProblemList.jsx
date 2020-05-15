c71598f46f55c45876eb911df6928c99afd7b434
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-16 01:01:16 +0900

    create OldMyAnswer component and ProblemResultView'

--- s-with-me-front/src/components/student/problem/ProblemList.jsx
+++ s-with-me-front/src/components/student/problem/ProblemList.jsx
@@ -4,6 +4,7 @@ import VerticalList from '../../../common-ui/VerticalList';
 import ProblemView from './ProblemView';
 
 import Api from '../../../Api';
+import ProblemResultView from './ProblemResultView';
 
 export default class ProblemList extends PureComponent {
   static defaultProps = {
@@ -16,20 +17,34 @@ export default class ProblemList extends PureComponent {
   }
   render() {
     const { myProblemList } = this.props;
-    console.log(myProblemList);
-
     return (
       <div style={{ display: 'flex', flexDirection: 'row' }}>
         <div style={{ flex: 1, padding: 3 }}>
           <VerticalList spacingBetween={10}>
-            {myProblemList.map(({ problem, myProblemId }) => (
-              <ProblemView
-                problemNum={problem.problemNum}
-                content={problem.content}
-                isOptional={problem.isOptional}
-                myProblemId={myProblemId}
-              />
-            ))}
+            {myProblemList.map(({ problem, myProblemId, myAnswer, isConfused }) => {
+              if (myAnswer) {
+                return (
+                  <ProblemResultView
+                    problemNum={problem.problemNum}
+                    content={problem.content}
+                    isOptional={problem.isOptional}
+                    myProblemId={myProblemId}
+                    isRight={problem.answer == myAnswer ? true : false}
+                    myAnswer={myAnswer}
+                    isConfused={isConfused}
+                  />
+                );
+              } else {
+                return (
+                  <ProblemView
+                    problemNum={problem.problemNum}
+                    content={problem.content}
+                    isOptional={problem.isOptional}
+                    myProblemId={myProblemId}
+                  />
+                );
+              }
+            })}
           </VerticalList>
         </div>
         <div style={{ flex: 1, padding: 3 }}>
