55809a967fee22646f23e1751d492f74e709721b
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-19 01:48:20 +0900

    notification 숨기기 (debounce)

--- s-with-me-front/src/middlewares/notificationEffects.js
+++ s-with-me-front/src/middlewares/notificationEffects.js
@@ -1,11 +1,18 @@
 import { SET_ERROR } from '../actions/myProblemActions';
-import { showMessage } from '../actions/notificationActions';
+import { showMessage, SHOW_NOTIFICATION, hideMessage } from '../actions/notificationActions';
+import { debounce } from '../debounce';
+
+const debounceRunner = debounce(action => action(), 4000);
 
 export default store => nextRunner => action => {
   const { type, payload } = action;
   if (type === SET_ERROR) {
     const { errorMessage } = payload;
     store.dispatch(showMessage(errorMessage, true));
+  } else if (type === SHOW_NOTIFICATION) {
+    const hide = () => store.dispatch(hideMessage());
+    setTimeout(4000);
+    debounceRunner(hide);
   }
   return nextRunner(action);
 };
