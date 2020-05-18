b58176dc675b108db7c62c9190ef1382337411f0
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-19 01:03:44 +0900

    error server설치 (error handling을 위해)

--- s-with-me-front/src/Api.js
+++ s-with-me-front/src/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: 'http://ec2-3-34-84-81.ap-northeast-2.compute.amazonaws.com:8085',
+  // baseURL: 'http://ec2-3-34-84-81.ap-northeast-2.compute.amazonaws.com:8085',
+  baseURL: 'http://localhost:4000',
 });
 
 export default Api;
