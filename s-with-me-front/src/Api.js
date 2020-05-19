cc697453a038bffff0bb4c3085864b4e2f8e884b
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-19 23:30:30 +0900

    change base URL

--- s-with-me-front/src/Api.js
+++ s-with-me-front/src/Api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  // baseURL: 'http://ec2-3-34-84-81.ap-northeast-2.compute.amazonaws.com:8085',
-  baseURL: 'http://localhost:4000',
+  baseURL: 'http://ec2-3-34-84-81.ap-northeast-2.compute.amazonaws.com:8085',
 });
 
 export default Api;
