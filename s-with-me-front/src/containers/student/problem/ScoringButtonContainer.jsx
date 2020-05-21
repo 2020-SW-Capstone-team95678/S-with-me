18f1df6f77b46fcc08b2d57a293c4a460802d889
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-21 14:58:17 +0900

    setLastMyProblem action -> ScoringButton에 연결

--- s-with-me-front/src/containers/student/problem/ScoringButtonContainer.jsx
+++ s-with-me-front/src/containers/student/problem/ScoringButtonContainer.jsx
@@ -1,11 +1,13 @@
 import { connect } from 'react-redux';
 import { setSolvedDateTime, setIsRight } from '../../../actions/myProblemActions';
 import ScoringButton from '../../../components/student/problem/ScoringButton';
+import { setLastMyProblem } from '../../../actions/myBookActions';
 
 const mapDispatchToProps = dispatch => {
   return {
     setSolvedDateTime: (id, solvedDateTime) => dispatch(setSolvedDateTime(id, solvedDateTime)),
     setIsRight: (id, isRight) => dispatch(setIsRight(id, isRight)),
+    setLastMyProblem: (id, lastMyProblemId) => dispatch(setLastMyProblem(id, lastMyProblemId)),
   };
 };
 
