82a7c0bb9e97d7b8a5ba314dc5364edf48ffab04
JungHun <youngsangsid@naver.com>
2020-05-21 17:26:19 +0900

    Signup , Login 기능 CSRF 토큰을 사용한 세션 방식에서 프론트와의 협업을 위해 JWT 방식으로 변경 회원가입 , 로그인 , 회원 아이디 변경 기능 구현 완료

--- s-with-me-back/src/main/java/com/swithme/domain/student/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.swithme.domain.student;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface StudentRepository extends JpaRepository<Student, String> {
-    Optional<Student> findByUserId(String userId);
-}
--- s-with-me-back/src/main/java/com/swithme/web/dto/StudentCreateDto.java
+++ s-with-me-back/src/main/java/com/swithme/web/dto/StudentCreateDto.java
@@ -3,16 +3,13 @@ package com.swithme.web.dto;
 import com.swithme.domain.student.Student;
 import lombok.*;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-public class StudentDto {
+public class StudentCreateDto {
     private int studentId;
     private String userId;
-
     private String password;
     private String name;
     private String phoneNumber;
@@ -36,10 +33,9 @@ public class StudentDto {
     }
 
     @Builder
-    public StudentDto(int studentId,String userId, String password, String name , String phoneNumber ,String birthday , Short grade) {
+    public StudentCreateDto(int studentId, String userId, String password, String name , String phoneNumber , String birthday , Short grade) {
         this.studentId = studentId;
         this.userId = userId;
-
         this.password = password;
         this.name = name;
         this.phoneNumber = phoneNumber;
