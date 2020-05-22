221d8aedbb6cbed359fc7cb3ba1bb39a8f89e447
JungHun <youngsangsid@naver.com>
2020-05-22 13:42:17 +0900

    폴더 수정 기능 구현

--- s-with-me-back/src/main/java/com/swithme/web/controller/FolderController.java
+++ s-with-me-back/src/main/java/com/swithme/web/controller/FolderController.java
@@ -2,10 +2,10 @@ package com.swithme.web.controller;
 
 import com.swithme.service.FolderService;
 import com.swithme.web.dto.FolderCreateDto;
+import com.swithme.web.dto.FolderUpdateRequestDto;
+import com.swithme.web.dto.StudentUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,4 +19,9 @@ public class FolderController {
         return folderCreateDto.getFolderName();
     }
 
+    @CrossOrigin
+    @PutMapping("/student/library/folder")
+    public int updateFolder(int folderId , FolderUpdateRequestDto folderUpdateRequestDto){
+        return folderService.updatefolder(folderId, folderUpdateRequestDto);
+    }
 }
---
+++ s-with-me-back/src/main/java/com/swithme/web/dto/FolderUpdateRequestDto.java
@@ -0,0 +1,16 @@
+package com.swithme.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class FolderUpdateRequestDto {
+    String folderName;
+
+    @Builder
+    public FolderUpdateRequestDto( String folderName) {
+        this. folderName = folderName;
+    }
+}
