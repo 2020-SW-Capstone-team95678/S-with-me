ecd37ba2f9f3266f9cc6d3212bcf727dba7f8818
sungkyu <kskyu610@gmail.com>
2020-05-23 21:46:35 +0900

    GET /publisher/library?publisherId={publisherId} - 문제 리스트 가져오기

--- s-with-me-back/src/main/java/com/swithme/service/BookService.java
+++ s-with-me-back/src/main/java/com/swithme/service/BookService.java
@@ -2,19 +2,25 @@ package com.swithme.service;
 
 import com.swithme.domain.book.Book;
 import com.swithme.domain.book.BookRepository;
+import com.swithme.domain.publisher.Publisher;
+import com.swithme.domain.publisher.PublisherRepository;
 import com.swithme.web.dto.BookResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
     @Transactional
-    public BookResponseDto getBook(int bookId){
+    public BookResponseDto getBookInformation(int bookId){
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 book이 없습니다. bookId = " + bookId));
         BookResponseDto responseDto = BookResponseDto.builder()
@@ -32,5 +38,30 @@ public class BookService {
                 .monthlySold(book.getMonthlySold())
                 .build();
         return responseDto;
+    }
+
+    @Transactional
+    public List<BookResponseDto> getBookList(int publisherId) {
+        Publisher publisher = publisherRepository.findById(publisherId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 publisher가 없습니다. publisherId = " + publisherId));
+        List<Book> bookList = bookRepository.findByPublisher(publisher);
+        List<BookResponseDto> responseDtoList = new ArrayList<>();
+        for(Book book : bookList){
+            responseDtoList.add(BookResponseDto.builder()
+                    .bookId(book.getBookId())
+                    .publisherId(publisherId)
+                    .subject(book.getSubject())
+                    .price(book.getPrice())
+                    .publishedDate(book.getPublishedDate())
+                    .name(book.getName())
+                    .grade(book.getGrade())
+                    .cover(book.getCover())
+                    .isAdvertised(book.getIsAdvertised())
+                    .totalProblemNumber(book.getTotalProblemNumber())
+                    .monthlyProfit(book.getMonthlyProfit())
+                    .monthlySold(book.getMonthlySold())
+                    .build());
+        }
+        return responseDtoList;
     }
 }
--- s-with-me-back/src/main/java/com/swithme/web/controller/BookController.java
+++ s-with-me-back/src/main/java/com/swithme/web/controller/BookController.java
@@ -3,10 +3,8 @@ package com.swithme.web.controller;
 import com.swithme.service.BookService;
 import com.swithme.web.dto.BookResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,7 +14,13 @@ public class BookController {
 
     @CrossOrigin
     @GetMapping("/student/library/my-book")
-    public BookResponseDto getBook(@RequestParam("bookId") int bookId){
-        return bookService.getBook(bookId);
+    public BookResponseDto getBookInformation(@RequestParam("bookId") int bookId){
+        return bookService.getBookInformation(bookId);
+    }
+
+    @CrossOrigin
+    @GetMapping("/publisher/library")
+    public List<BookResponseDto> getBookList(@RequestParam("publisherId") int publisherId){
+        return bookService.getBookList(publisherId);
     }
 }
