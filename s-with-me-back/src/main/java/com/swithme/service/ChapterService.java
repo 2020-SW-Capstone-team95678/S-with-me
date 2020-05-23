09404d491d87fb6a2a33b6358aeeb9a466a15816
sungkyu <kskyu610@gmail.com>
2020-05-24 01:16:09 +0900

    챕터 dto에 book 객체가 들어가는 문제 수정

--- s-with-me-back/src/main/java/com/swithme/service/ChapterService.java
+++ s-with-me-back/src/main/java/com/swithme/service/ChapterService.java
@@ -7,6 +7,8 @@ import com.swithme.domain.mainChapter.MainChapterRepository;
 import com.swithme.domain.subChapter.SubChapter;
 import com.swithme.domain.subChapter.SubChapterRepository;
 import com.swithme.web.dto.ChapterResponseDto;
+import com.swithme.web.dto.MainChapterResponseDto;
+import com.swithme.web.dto.SubChapterResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -23,12 +25,27 @@ public class ChapterService {
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 book이 없습니다. bookId = " + bookId));
         List<ChapterResponseDto> responseDtoList = new ArrayList<>();
+
         List<MainChapter> mainChapterList = mainChapterRepository.findByBook(book);
         for(MainChapter mainChapter : mainChapterList){
+            MainChapterResponseDto mainChapterResponseDto = MainChapterResponseDto.builder()
+                    .mainChapterId(mainChapter.getMainChapterId())
+                    .mainChapterName(mainChapter.getMainChapterName())
+                    .build();
+
             List<SubChapter> subChapterList = subChapterRepository.findByMainChapter(mainChapter);
+            List<SubChapterResponseDto> subChapterResponseDtoList = new ArrayList<>();
+            for(SubChapter subChapter : subChapterList){
+                subChapterResponseDtoList.add(SubChapterResponseDto.builder()
+                        .mainChapterId(mainChapter.getMainChapterId())
+                        .subChapterId(subChapter.getSubChapterId())
+                        .subChapterName(subChapter.getSubChapterName())
+                        .build());
+            }
+
             responseDtoList.add(ChapterResponseDto.builder()
-                    .mainChapter(mainChapter)
-                    .subChapterList(subChapterList)
+                    .mainChapterResponseDto(mainChapterResponseDto)
+                    .subChapterResponseDtoList(subChapterResponseDtoList)
                     .build());
         }
         return responseDtoList;
