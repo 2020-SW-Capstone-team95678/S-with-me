ce1f9c18dbc991f9f6a7176fe255662042a4b8e8
JungHun <youngsangsid@naver.com>
2020-05-25 16:10:58 +0900

    수정

--- s-with-me-back/src/main/java/com/swithme/web/dto/MyBookResponseDto.java
+++ s-with-me-back/src/main/java/com/swithme/web/dto/MyBookResponseDto.java
@@ -11,13 +11,13 @@ public class MyBookResponseDto {
     private int myBookId;
     private int folderId;
     private int bookId;
-    private short lastPageNumber;
+    private short lastProblemNumber;
 
     @Builder
-    public MyBookResponseDto(int myBookId, int folderId, int bookId, short lastPageNumber){
+    public MyBookResponseDto(int myBookId, int folderId, int bookId, short lastProblemNumber){
         this.myBookId = myBookId;
         this.folderId = folderId;
         this.bookId = bookId;
-        this.lastPageNumber = lastPageNumber;
+        this.lastProblemNumber = lastProblemNumber;
     }
 }
