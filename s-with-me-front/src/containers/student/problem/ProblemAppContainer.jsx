569db704ab349173cbc006783ad5b861761a2524
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-28 17:19:29 +0900

    props name number->page로 변경 / MyProblemList->ProblemAppContainer (ProblemApp에서 request)

--- s-with-me-front/src/containers/student/problem/ProblemAppContainer.jsx
+++ s-with-me-front/src/containers/student/problem/ProblemAppContainer.jsx
@@ -1,17 +1,18 @@
 import { connect } from 'react-redux';
 import { requestMyProblemList, FETCH_MY_PROBLEM_LIST } from '../../../actions/myProblemPackActions';
-import ProblemList from '../../../components/student/problem/ProblemList';
+import ProblemApp from '../../../components/student/problem/ProblemApp';
 
 const mapStateToProps = state => {
-  const { ids, entities, loadingState } = state.myProblemList;
+  const { ids, entities, pagination, loadingState } = state.myProblemList;
   const loading = loadingState[FETCH_MY_PROBLEM_LIST];
   const myProblemList = ids.map(id => entities[id]);
+  const { number } = pagination;
 
-  return { myProblemList, loading };
+  return { myProblemList, loading, number };
 };
 
 const mapDispatchToProps = {
   requestMyProblemList,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProblemList);
+export default connect(mapStateToProps, mapDispatchToProps)(ProblemApp);
