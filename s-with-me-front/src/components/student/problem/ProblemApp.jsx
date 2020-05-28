a891dc152816e0fdf744de2dc3d684c15629bceb
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-28 17:53:12 +0900

    myProblemList filtering

--- s-with-me-front/src/components/student/problem/ProblemApp.jsx
+++ s-with-me-front/src/components/student/problem/ProblemApp.jsx
@@ -20,6 +20,7 @@ export default class ProblemApp extends PureComponent {
   render() {
     const { myBookId, subChapterId } = this.props.match.params;
     const { number, myProblemList, loading, viewWrongOnly } = this.props;
+    const onlyWrongMyProblemList = myProblemList.filter(myProblem => myProblem.isRight === false);
     return (
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <div style={{ hight: 5, padding: 3 }}>
@@ -29,7 +30,7 @@ export default class ProblemApp extends PureComponent {
           <ProblemList
             subChapterId={subChapterId}
             page={number}
-            myProblemList={viewWrongOnly ? null : myProblemList}
+            myProblemList={viewWrongOnly ? onlyWrongMyProblemList : myProblemList}
             loading={loading}
           />
         </div>
