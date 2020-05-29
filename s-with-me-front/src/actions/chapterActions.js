4b9d750296030311f929b20bc0748e7c0e63ced1
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-29 22:57:45 +0900

    로그아웃 오류 해결 / chapterList Api 변경

--- s-with-me-front/src/actions/chapterActions.js
+++ s-with-me-front/src/actions/chapterActions.js
@@ -3,9 +3,10 @@ import Api from '../Api';
 export const FETCH_CHAPTER_LIST = 'chapter/FETCH_CHAPTER_LIST';
 
 export function requestChapterList(params) {
+  const bookId = params.bookId;
   return {
     type: FETCH_CHAPTER_LIST,
-    promise: Api.get('/student/library/my-book/chapters', { params: params }),
+    promise: Api.get(`/library/book/${bookId}/chapters`),
     meta: {
       notification: {
         error: '목차를 불러오는 중에 문제가 발생했습니다.',
