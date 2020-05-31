f0bfe12f3c48ac4781da5a54f8db7d04715403f4
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-31 19:34:28 +0900

    curriculum 설정 페이지 ui

--- s-with-me-front/src/ModalProvider.jsx
+++ s-with-me-front/src/ModalProvider.jsx
@@ -7,6 +7,7 @@ import {
   CREATE_FOLDER,
   DELETE_FOLDER,
   UPDATE_FOLDER_NAME,
+  CREATE_CURRICULUM,
 } from './constants/modals';
 import ProfileEditPage from './components/publisher/profile/ProfileEditPage';
 import ProfileEditPageStudent from './components/student/profile/ProfileEditPage';
@@ -14,6 +15,7 @@ import CreateFolderPageContainer from './containers/student/book/CreateFolderPag
 import DeleteFolderPageContainer from './containers/student/book/DeleteFolderPageContainer';
 import UpdateFolderNamePageContainer from './containers/student/book/UpdateFolderNamePageContainer';
 import DeleteNoteContainer from './containers/student/note/DeleteNoteContainer';
+import CreateCurriculumPageContainer from './containers/student/book/CreateCurriculumPageContainer';
 
 export default createProvider({
   [P_PROFILE_EDIT_MODAL]: ProfileEditPage,
@@ -22,4 +24,5 @@ export default createProvider({
   [CREATE_FOLDER]: CreateFolderPageContainer,
   [DELETE_FOLDER]: DeleteFolderPageContainer,
   [UPDATE_FOLDER_NAME]: UpdateFolderNamePageContainer,
+  [CREATE_CURRICULUM]: CreateCurriculumPageContainer,
 });
---
+++ s-with-me-front/src/components/student/libarary/CreateCurriculumPage.jsx
@@ -0,0 +1,91 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import { Consumer as Modal } from '../../../common-ui/Modal/context';
+import Form from '../../../common-ui/Form';
+import Spacing from '../../../common-ui/Spacing';
+import Text from '../../../common-ui/Text';
+import Select, { Option } from '../../../common-ui/Select';
+import Input from '../../../common-ui/Input';
+import InlineList from '../../../common-ui/InlineList';
+import Button from '../../../common-ui/Button';
+import SelectSubChapter from './SelectSubChapter';
+
+export default class CreateCurriculumPage extends PureComponent {
+  static propTypes = {
+    createCurriculum: PropTypes.func,
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(values, closeModal) {
+    const { bookId } = this.props;
+    const formValue = {
+      type: values.curriculumType,
+      goalNumber: values.curriculumGoalNumber,
+    };
+    console.log(bookId, formValue);
+    console.log(values.selectSubChapterId);
+  }
+
+  render() {
+    return (
+      <Modal>
+        {({ closeModal }) => (
+          <Form onSubmit={values => this.handleSubmit(values, closeModal)}>
+            <Form.Consumer>
+              {({ onChange, values }) => (
+                <Spacing horizontal={4} vertical={8}>
+                  <Text xlarge bold>
+                    커리큘럼 설정
+                  </Text>
+                  <Spacing bottom={2}>
+                    <Select name="curriculumType" onChange={onChange}>
+                      <Option label="선택해 주세요" value="" />
+                      <Option label="월별 커리큘럼" value="monthly" />
+                      <Option label="주별 커리큘럼" value="weekly" />
+                      <Option label="일별 커리큘럼" value="daily" />
+                    </Select>
+                  </Spacing>
+                  {values.curriculumType === 'daily' ? (
+                    <Spacing bottom={2}>
+                      <Text>매일 풀 목표 문제 수를 입력해주세요!</Text>
+                      <Input name="curriculumGoalNumber" onChange={onChange} type="number" />
+                    </Spacing>
+                  ) : values.curriculumType === 'weekly' ? (
+                    <Spacing bottom={2}>
+                      <Text large>이번 주에 풀 소단원을 선택해주세요!</Text>
+                      <div style={{ display: 'flex' }}>
+                        <Select name="selectMainChapterId" onChange={onChange}>
+                          <Option label="선택해 주세요" value="" />
+                          {this.props.chapterList.map(({ mainChapter }) => (
+                            <Option
+                              label={mainChapter.mainChapterName}
+                              value={mainChapter.mainChapterId}
+                            />
+                          ))}
+                        </Select>
+                        <SelectSubChapter
+                          chapterList={this.props.chapterList}
+                          mainChapterId={values.selectMainChapterId}
+                          onChange={onChange}
+                        />
+                      </div>
+                    </Spacing>
+                  ) : null}
+                  <InlineList spacingBetween={1}>
+                    <Button primary>설정</Button>
+                    <Button onPress={closeModal}>취소</Button>
+                  </InlineList>
+                </Spacing>
+              )}
+            </Form.Consumer>
+          </Form>
+        )}
+      </Modal>
+    );
+  }
+}
