69c9f2b4020ff2e89617b761b2e2b4a1dd4e619f
surin-0 <marblegiraffe@ajou.ac.kr>
2020-05-31 16:13:49 +0900

    문제집 커버 toggle

--- s-with-me-front/src/components/student/libarary/BookDetail.jsx
+++ s-with-me-front/src/components/student/libarary/BookDetail.jsx
@@ -5,28 +5,61 @@ import Heading from '../../../common-ui/Heading';
 import Button from '../../../common-ui/Button';
 import ChapterList from './ChapterList';
 import { Link } from 'react-router-dom';
+import Api from '../../../Api';
 
 export default class BookDetail extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { subject: '', name: '', grade: -1, cover: null, isCoverClicked: false };
+  }
+
   componentDidMount() {
     const { requestChapterList, myBook } = this.props;
     requestChapterList({ bookId: myBook.bookId });
+    Api.get('/student/library/my-book', { params: { bookId: myBook.bookId } }).then(({ data }) =>
+      this.setState({
+        subject: data.subject,
+        name: data.name,
+        grade: data.grade,
+        cover: data.cover,
+      }),
+    );
   }
 
   render() {
     const { chapterList, myBook } = this.props;
     const { myBookId } = this.props.match.params;
     const { lastPageNumber, lastSubChapterId } = myBook;
+    const { subject, name, grade, cover, isCoverClicked } = this.state;
     return (
       <div style={{ display: 'flex' }}>
         <div style={{ flex: 1, flexDirection: 'row', padding: 3 }}>
-          <div
-            style={{
-              height: 100,
-              flexDirection: 'column',
-              padding: 3,
-              border: '1px red solid',
-            }}
-          ></div>
+          {isCoverClicked ? (
+            <div
+              onClick={() => this.setState({ isCoverClicked: false })}
+              style={{
+                height: 100,
+                flexDirection: 'column',
+                padding: 3,
+                border: '1px red solid',
+              }}
+            >
+              {grade}학년 / 과목 : {subject} <br />
+              이름: {name}
+            </div>
+          ) : (
+            <div
+              onClick={() => this.setState({ isCoverClicked: true })}
+              style={{
+                height: 100,
+                flexDirection: 'column',
+                padding: 3,
+                border: '1px red solid',
+              }}
+            >
+              {cover}
+            </div>
+          )}
           <div style={{ flex: 1, flexDirection: 'column', padding: 3 }}>
             <Card>
               <Heading level={3}>목표</Heading>
