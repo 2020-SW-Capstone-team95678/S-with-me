66d820de899d806ae1f8ab1e5b6eb07f7f86da8e
surin-0 <marblegiraffe@ajou.ac.kr>
2020-06-03 01:32:12 +0900

    LibraryApp에서 curriculumList 가져오기

--- s-with-me-front/src/components/student/libarary/LibraryApp.jsx
+++ s-with-me-front/src/components/student/libarary/LibraryApp.jsx
@@ -6,24 +6,29 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 import Card from '../../../common-ui/Card';
 import LibraryFilterList from './LibraryFilterList';
 import BookOverview from './BookOverview';
+import CurriculumList from './CurriculumList';
 
 class LibraryApp extends PureComponent {
   static defaultProps = {
     myBookList: [],
     folderList: [],
+    curriculumList: [],
     requestMyBookList: () => {},
     requestFolderList: () => {},
+    requestCurriculumList: () => {},
   };
 
   componentDidMount() {
-    const { requestMyBookList, requestFolderList } = this.props;
+    const { requestMyBookList, requestFolderList, requestCurriculumList } = this.props;
     const studentId = window.sessionStorage.getItem('studentId');
     requestMyBookList({ studentId: studentId });
     requestFolderList({ studentId: studentId });
+    requestCurriculumList({ studentId: studentId });
   }
 
   render() {
     const { myBookList, bookListLoading, folderLoading, folderList, styles } = this.props;
+    const { curriculumList } = this.props;
     return (
       <DndProvider backend={HTML5Backend}>
         <div style={{ display: 'flex' }}>
@@ -44,8 +49,9 @@ class LibraryApp extends PureComponent {
             style={{ flex: 1, border: '1px solid', borderColor: '#D9CBC7', borderRadius: '0.5rem' }}
           >
             <Card vertical={4}>
-              <div {...css(styles.head)}>이달의 목표</div>
+              <div {...css(styles.head)}>나의 커리큘럼</div>
             </Card>
+            <CurriculumList curriculumList={curriculumList} />
           </div>
         </div>
       </DndProvider>
