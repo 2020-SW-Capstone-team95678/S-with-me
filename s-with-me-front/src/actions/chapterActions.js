76356218d25d400a905ff1bedd04823b598448f0
surin-0 <marblegiraffe@ajou.ac.kr>
2020-06-03 04:35:34 +0900

    pagination, chapterList 등 여러 버그 수정

--- s-with-me-front/src/actions/chapterActions.js
+++ s-with-me-front/src/actions/chapterActions.js
@@ -3,10 +3,9 @@ import Api from '../Api';
 export const FETCH_CHAPTER_LIST = 'chapter/FETCH_CHAPTER_LIST';
 
 export function requestChapterList(params) {
-  const id = params.myBookId;
   return {
     type: FETCH_CHAPTER_LIST,
-    promise: Api.get(`/library/book/${id}/chapters`),
+    promise: Api.get('/student/library/my-book/chapters', { params: params }),
     meta: {
       notification: {
         error: '목차를 불러오는 중에 문제가 발생했습니다.',
