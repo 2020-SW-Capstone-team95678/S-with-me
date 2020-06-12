a5a206fd11e3222b700e987058308cd879674519
surin-0 <marblegiraffe@ajou.ac.kr>
2020-06-12 15:49:14 +0900

    remove storybook

--- s-with-me-front/src/stories/NoteStory.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import NoteApp from '../components/student/note/NoteApp';
-
-storiesOf('Note/NoteApp', module).addWithJSX('Note App', () => <NoteApp />);
