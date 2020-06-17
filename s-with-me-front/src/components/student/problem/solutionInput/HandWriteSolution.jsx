0d6e5079a674752ca8d549eefcee9f60ca078229
ssoheej <wjdthgmlek@ajou.ac.kr>
2020-06-18 08:22:22 +0900

    mobile도 적용

--- s-with-me-front/src/components/student/problem/solutionInput/HandWriteSolution.jsx
+++ s-with-me-front/src/components/student/problem/solutionInput/HandWriteSolution.jsx
@@ -1,36 +1,41 @@
 import React from 'react';
-import CanvasDraw from "react-canvas-draw";
+import CanvasDraw from 'react-canvas-draw';
 
 //import { SketchField, Tools } from 'react-sketch';
 //import {ReactSketchCanvas} from 'react-sketch-canvas';
 import { Icon, Menu } from 'semantic-ui-react';
 import { Slider } from 'react-semantic-ui-range';
 
-
 class HandWriteSolution extends React.Component {
   constructor(props) {
     super(props);
     this.canvas = React.createRef();
-    
-    this.state = { drawings: [], activeItem: 'write',controlWeghit:false, lineWidth:3,value:0 };
+
+    this.state = {
+      drawings: [],
+      activeItem: 'write',
+      controlWeghit: false,
+      lineWidth: 3,
+      value: 0,
+    };
   }
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
-  handleLineWeghit = (e) => {
-      const {controlWeghit} = this.state;
-    this.setState({ controlWeghit: !controlWeghit});
+  handleLineWeghit = e => {
+    const { controlWeghit } = this.state;
+    this.setState({ controlWeghit: !controlWeghit });
     console.log(controlWeghit);
-};
+  };
   _save = () => {
     const { id, setHandSolution, isNote, setMyNewHandSolution } = this.props;
     //const drawings = this.saveableCanvas.toDataURL("image/png");
-    const drawings = this.canvas.current.canvasContainer.children[1].toDataURL()
+    const drawings = this.canvas.current.canvasContainer.children[1].toDataURL();
     //const drawings = this.saveableCanvas.getSaveData();
     console.log(drawings);
     if (isNote) setMyNewHandSolution(id, drawings);
     else setHandSolution(id, drawings);
   };
   render = () => {
-    const { activeItem,lineWidth,controlWeghit } = this.state;
+    const { activeItem, lineWidth, controlWeghit } = this.state;
     return (
       <div style={{ display: 'flex' }}>
         <div style={{ disply: 'flex', alignContent: 'center' }}>
@@ -43,34 +48,39 @@ class HandWriteSolution extends React.Component {
               <Icon name="eraser" />
               지우개
             </Menu.Item>
-            <Menu.Item  onClick={this.handleLineWeghit} key="Line">Line</Menu.Item>
-            {controlWeghit ? <Slider color="red" inverted={false}
+            <Menu.Item onClick={this.handleLineWeghit} key="Line">
+              Line
+            </Menu.Item>
+            {controlWeghit ? (
+              <Slider
+                color="red"
+                inverted={false}
                 settings={{
                   start: this.state.lineWidth,
                   min: 0,
                   max: 10,
                   step: 1,
-                  onChange: (value) => {
+                  onChange: value => {
                     this.setState({
-                      lineWidth: value
-                    })
-                  }
-                }} />:null}
-
+                      lineWidth: value,
+                    });
+                  },
+                }}
+              />
+            ) : null}
           </Menu>
         </div>
-        
-        <CanvasDraw 
-        ref={this.canvas}
-        brushColor={activeItem === 'write' ? 'black' : 'white'}
-        brushRadius={lineWidth}
-        lazyRadius="0"
-        gridColor='white'
-        canvasWidth="100%"
-        canvasHeight="200"
-        onChange={this._save}
-        />
 
+        <CanvasDraw
+          ref={this.canvas}
+          brushColor={activeItem === 'write' ? 'black' : 'white'}
+          brushRadius={lineWidth}
+          lazyRadius="0"
+          gridColor="white"
+          canvasWidth="100%"
+          canvasHeight="200"
+          onChange={this._save}
+        />
       </div>
     );
   };
--- s-with-me-front/src/components/student/problem/solutionInput/MobileHandWriteSolutionInput.jsx
+++ s-with-me-front/src/components/student/problem/solutionInput/MobileHandWriteSolutionInput.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import { WithStore } from 'pure-react-carousel';
-import { SketchField, Tools } from 'react-sketch';
+import CanvasDraw from 'react-canvas-draw';
 import { Icon, Menu } from 'semantic-ui-react';
 import { Slider } from 'react-semantic-ui-range';
 
 class MobileHandWriteSolution extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { drawings: [], activeItem: 'write',controlWeghit:false, lineWidth:3,value:0};
+    this.state = {
+      drawings: [],
+      activeItem: 'write',
+      controlWeghit: false,
+      lineWidth: 3,
+      value: 0,
+    };
   }
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
-  handleLineWeghit = (e) => {
-    const {controlWeghit} = this.state;
-  this.setState({ controlWeghit: !controlWeghit});
+  handleLineWeghit = e => {
+    const { controlWeghit } = this.state;
+    this.setState({ controlWeghit: !controlWeghit });
   };
   _save = () => {
     const { id, setHandSolution, isNote, setMyNewHandSolution } = this.props;
-    const drawings = this._sketch.toDataURL('image/png');
-    //const drawData = JSON.stringify(drawings);
+    //const drawings = this.saveableCanvas.toDataURL("image/png");
+    const drawings = this.canvas.current.canvasContainer.children[1].toDataURL();
+    //const drawings = this.saveableCanvas.getSaveData();
+    //console.log(drawings);
     if (isNote) setMyNewHandSolution(id, drawings);
     else setHandSolution(id, drawings);
   };
   render = () => {
-    const { activeItem,lineWidth,controlWeghit } = this.state;
+    const { activeItem, lineWidth, controlWeghit } = this.state;
     return (
       <div
         style={{ display: 'flex' }}
@@ -39,30 +47,36 @@ class MobileHandWriteSolution extends React.Component {
               <Icon name="eraser" />
               지우개
             </Menu.Item>
-            <Menu.Item value={Tools.Line} onClick={this.handleLineWeghit} key="Line">Line</Menu.Item>
-            {controlWeghit ? <Slider color="red" inverted={false}
+            <Menu.Item value={Tools.Line} onClick={this.handleLineWeghit} key="Line">
+              Line
+            </Menu.Item>
+            {controlWeghit ? (
+              <Slider
+                color="red"
+                inverted={false}
                 settings={{
                   start: this.state.lineWidth,
                   min: 0,
                   max: 10,
                   step: 1,
-                  onChange: (value) => {
+                  onChange: value => {
                     this.setState({
-                      lineWidth: value
-                    })
-                  }
-                }} />:null}
+                      lineWidth: value,
+                    });
+                  },
+                }}
+              />
+            ) : null}
           </Menu>
         </div>
-        <SketchField
-          name="sketch"
-          className="canvas-area"
-          ref={c => (this._sketch = c)}
-          width="100%"
-          height="50%"
-          tool={Tools.Pencil}
-          lineColor={activeItem === 'write' ? 'black' : 'white'}
-          lineWidth={lineWidth}
+        <CanvasDraw
+          ref={this.canvas}
+          brushColor={activeItem === 'write' ? 'black' : 'white'}
+          brushRadius={lineWidth}
+          lazyRadius="0"
+          gridColor="white"
+          canvasWidth="100%"
+          canvasHeight="200"
           onChange={this._save}
         />
       </div>
