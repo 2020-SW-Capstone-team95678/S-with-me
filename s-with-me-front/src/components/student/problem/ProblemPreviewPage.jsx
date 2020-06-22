7ef66679402eac25190b7f4536eee1ebb4b16467
surin-0 <marblegiraffe@ajou.ac.kr>
2020-06-22 23:18:38 +0900

    링크 solution  input 부분  ui 개선

--- s-with-me-front/src/components/student/problem/ProblemPreviewPage.jsx
+++ s-with-me-front/src/components/student/problem/ProblemPreviewPage.jsx
@@ -1,10 +1,10 @@
 import React, { PureComponent } from 'react';
 
-import Spacing from '../../../common-ui/Spacing';
 import Text from '../../../common-ui/Text';
-import Button from '../../../common-ui/Button';
+
 import VerticalList from '../../../common-ui/VerticalList';
-import { Consumer as Modal } from '../../../common-ui/Modal/context';
+
+import { Modal, Segment, Divider, Button, Label } from 'semantic-ui-react';
 import Api from '../../../Api';
 
 export default class ProblemPreviewPage extends PureComponent {
@@ -12,29 +12,35 @@ export default class ProblemPreviewPage extends PureComponent {
     super(props);
     this.state = {
       problem: {},
-      myAnswer: null,
-      solutionType: null,
-      textSolution: null,
-      imageSolution: null,
-      linkSolutionId: null,
+      myProblemId: null,
+      open: false,
+      shouldUpdate: false,
     };
   }
-  componentDidMount() {
-    Api.get('/student/library/my-book/my-problem/problem-id', {
-      params: { myProblemId: this.props.myProblemId },
-    }).then(({ data }) =>
-      this.setState({
-        problem: data.problem,
-        myAnswer: data.myAnswer,
-        solutionType: data.solutionType,
-        textSolution: data.textSolution,
-        imageSolution: data.imageSolution,
-      }),
-    );
+  show = () => this.setState({ open: true });
+  close = () => this.setState({ open: false });
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.myProblemId !== prevState.myProblemId) {
+      return { shouldUpdate: true };
+    }
+    return { shouldUpdate: false };
   }
 
+  componentDidUpdate() {
+    if (this.state.shouldUpdate) {
+      Api.get('/student/library/my-book/my-problem/problem-id', {
+        params: { myProblemId: this.props.myProblemId },
+      }).then(({ data }) =>
+        this.setState({
+          problem: data.problem,
+          myProblemId: this.props.myProblemId,
+        }),
+      );
+    }
+  }
   render() {
-    const { myAnswer, solutionType, textSolution, imageSolution, problem } = this.state;
+    const { problem } = this.state;
     const { problemNumber, content, isOptional, answer, solution, image, title } = problem;
     let optionContents = [];
     let numbers = ['①', '②', '③', '④', '⑤'];
@@ -46,57 +52,63 @@ export default class ProblemPreviewPage extends PureComponent {
       optionContents.push(problem.option5);
     }
     return (
-      <Modal>
-        {({ closeModal }) => (
-          <Spacing horizontal={4} vertial={8}>
-            <Text large>
-              {problemNumber ? problemNumber + '.' : null}
-              {title}
-            </Text>
-            {image ? (
-              <img
-                src={image}
-                alt={problemNumber + '문제 그림'}
-                style={{ width: '100%', height: 'auto' }}
-              />
-            ) : null}
-            {content ? (
-              <div style={{ border: '0.5px solid', padding: 2 }}>
-                <Text>{content}</Text>
-              </div>
-            ) : null}
-            {isOptional ? (
-              <VerticalList spacingBetween={1}>
-                {optionContents.map((option, index) => (
-                  <Text key={index}>
-                    {numbers[index]} : {option}
-                  </Text>
-                ))}
-              </VerticalList>
-            ) : null}
-            <br />
-            <div>
+      <div style={{ paddingTop: 10 }}>
+        <Segment attached="top">
+          참고할 문제를 선택해 주세요
+          <Label
+            attached="top right"
+            basic
+            icon="eye"
+            content="미리 보기"
+            onClick={() => this.show()}
+          />
+        </Segment>
+        <Modal open={this.state.open} dimmer="inverted">
+          <Modal.Content>
+            <Segment>
+              <Text large>
+                {problemNumber ? problemNumber + '.' : null}
+                {title}
+              </Text>
+            </Segment>
+            <Divider />
+            <Segment>
+              {image ? (
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                  <img
+                    src={image}
+                    alt={problemNumber + '문제 그림'}
+                    style={{
+                      maxHeight: '30vh',
+                      minHeight: '10vh',
+                      width: 'auto',
+                      maxWidth: '100%',
+                    }}
+                  />
+                </div>
+              ) : null}
+              {content ? <Segment>{content}</Segment> : null}
+              {isOptional ? (
+                <VerticalList spacingBetween={1}>
+                  {optionContents.map((option, index) => (
+                    <Text key={index}>
+                      {numbers[index]} : {option}
+                    </Text>
+                  ))}
+                </VerticalList>
+              ) : null}
+            </Segment>
+            <Segment>
               {problemNumber ? <Text>정답: {answer}</Text> : null}
               <br />
               <Text>해설: {solution}</Text>
-            </div>
-            <div>---------------------------------</div>
-            <div>
-              {problemNumber ? <Text>나의 정답: {myAnswer}</Text> : null}
-              <br />
-              <Text>나의 풀이↓</Text> <br />
-              {solutionType === 'text' ? (
-                <Text>{textSolution}</Text>
-              ) : solutionType === 'img' ? (
-                <img src={imageSolution} alt="나의 이미지 풀이" />
-              ) : (
-                <Text>링크 풀이 입니다.</Text>
-              )}
-            </div>
-            <Button onPress={closeModal}>닫기</Button>
-          </Spacing>
-        )}
-      </Modal>
+            </Segment>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button basic content="닫기" color="red" icon="close" onClick={() => this.close()} />
+          </Modal.Actions>
+        </Modal>
+      </div>
     );
   }
 }
