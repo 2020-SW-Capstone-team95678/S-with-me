f0143551d0616fb60315bbf2e1e61f356ea72384
surin-0 <marblegiraffe@ajou.ac.kr>
2020-06-23 14:18:33 +0900

    boot pay cors header에 추가

--- s-with-me-front/src/Api.js
+++ s-with-me-front/src/Api.js
@@ -5,4 +5,7 @@ const Api = axios.create({
   // baseURL: 'http://ec2-3-34-84-81.ap-northeast-2.compute.amazonaws.com:8085',
 });
 
+export const BootPayApi = axios.create({
+  baseURL: 'https://api.bootpay.co.kr',
+});
 export default Api;
