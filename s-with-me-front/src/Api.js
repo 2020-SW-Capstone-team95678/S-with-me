a34a056787b669a3ef0f2a33921bc321f918073e
surin-0 <marblegiraffe@ajou.ac.kr>
2020-09-27 14:48:48 +0900

    change base URL (edit typo)

--- s-with-me-front/src/Api.js
+++ s-with-me-front/src/Api.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const Api = axios.create({
   // baseURL: 'https://swithme.ml',
-  baseURL: 'http://ec2-3-34-84-81.ap-northeast-2.compute.amazonaws.com:8080',
-  // baseURL: 'http://ec2-3-34-84-81.ap-northeast-2.compute.amazonaws.com:8085',
+  baseURL: 'http://ec2-3-34-81-212.ap-northeast-2.compute.amazonaws.com:8080',
 });
 
 export default Api;
